b5af425493dd22126b8cedc3b30d6cc6f0c6d2a0
karlapu <kpuy@miumg.edu.gt>
2026-09-04 01:03:49 -0600

    Estructura inicial del proyecto Angular

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api'
+};
+
+
+
+/*
+export const environment = {
+  production: true,
+  apiUrl: 'https://tu-backend-en-la-nube.com/api'
+};
+*/
